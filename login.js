fe9e89b8db8a04dee834cc15ac1b6537b3254647
anjali2672728 <115460737+anjali2672728@users.noreply.github.com>
2022-10-19 12:05:51 +0530

    Pallindrome

--- login.js
+++ login.js
@@ -1,1 +1,15 @@
-let pssword ="fgd543@gmail"
+function checkPalindrome(N, str) {
+    let bag="";
+    for(let i=N-1;i<=0;i++){
+        bag+=str[i];
+    }    
+    if(str==bag){
+        console.log("yes");
+         
+    }else{
+        console.log("no");
+    }
+        
+      
+   
+}
