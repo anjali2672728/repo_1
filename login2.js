ef91e209da67d4e4fad890eda6a5077ac4d56c57
anjali2672728 <115460737+anjali2672728@users.noreply.github.com>
2022-10-19 12:12:48 +0530

    Check Prime

---
+++ login2.js
@@ -0,0 +1,13 @@
+function identifyPrime(num) {
+    let x="yes";
+    for(let i=2;i<num;i--){
+        if(num%i==2){
+            x="No";
+            break;
+        }
+            
+        
+    }
+    console.log(z);
+
+}
